624dfa0de73640cdcc77d4400ac9ae5b37a5ca0e
owlnate17 <ndienes2@illinois.edu>
2024-10-27 18:42:03 +0000

    Camera?

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-st.title("🎈 My new app")
-st.write(
-    "fromcolab"
-)
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        return frame  
+
+
+st.title("EmojiGen")
+
+# Set up the video stream
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+
+# Button to capture the image
+if st.button("Capture"):
+    frame = webrtc_streamer.get_frame()  # Get the latest frame
+    if frame is not None:
+        st.image(frame.to_ndarray(), caption='Captured Image', use_column_width=True)
