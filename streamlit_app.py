59a5ab4acd85918f224efc9f6cab8716c8aafc58
jackhhu1 <Jackhhu1@gmail.com>
2024-11-04 11:41:24 +1100

    newline add

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def draw_emoji_page():
 
     st.markdown(
         """
-        Here, you can draw your desired emoji! :) 
+        Here, you can draw your desired emoji! :) \n
         Press the 'Download' button at the bottom of the canvas to update exported image.
         """
     )
